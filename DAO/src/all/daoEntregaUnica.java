c784febe9805b76dbafabf50db03353c1313802f
Renzo Delgado <renzo.delgadop.98@gmail.com>
2017-11-11 16:16:20 -0500

    CLASES DAO AGREGADAS

---
+++ DAO/src/all/daoEntregaUnica.java
@@ -0,0 +1,13 @@
+package all;
+
+public class daoEntregaUnica {
+	
+	public static daoEntregaUnica _Instancia;
+	private daoEntregaUnica() {}
+	public static daoEntregaUnica Instancia() {
+		if(_Instancia==null) {
+			_Instancia = new daoEntregaUnica();
+		}
+		return _Instancia;
+	}
+}
